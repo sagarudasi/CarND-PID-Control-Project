bb855d8de873f3d2de201ba24d47f5df23ee25fc
Sagar Udasi <sagarudasi@outlook.com>
2017-09-13 19:48:27 +0530

    Working code with PID values taken from course videos

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,27 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    // Set the initial error to 0
+    this->p_error = 0.0;
+    this->i_error = 0.0;
+    this->d_error = 0.0;
+    // Set multiplier (tau) for Proportional, Integral and Derivative errors
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    // Calculate the d_error by taking difference of current cte and previous cte (i.e. p_error)
+    this->d_error = cte - this->p_error;
+    // Set p_error after d_error so that we can use its previous value above
+    this->p_error = cte;
+    // Set i_error
+    this->i_error = this->i_error + cte;
 }
 
 double PID::TotalError() {
+    // Calculate and return the total error as per PID formulae
+    // -tau_p * CTE - tau_d * diff_CTE - tau_i * int_CTE
+    return (-1 * this->Kp * this->p_error) - this->Kd * this->d_error - this->Ki * this->i_error;
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,8 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  // Start with the values provided in the course lectures
+  pid.Init(0.2, 0.004, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,10 +59,12 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
-
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = 0.3;
